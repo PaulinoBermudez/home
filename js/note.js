92ae25d0a18ec2cf5260b6602a1a8632e197aaf4
chunche95 <37621958+chunche95@users.noreply.github.com>
2020-08-05 17:28:40 +0200

    JavaScript sheet of paper

--- js/note.js
+++ js/note.js
@@ -1,1 +1,35 @@
-function getCookie(e){var n,t=document.cookie,o=t.indexOf(" "+e+"=");return-1==o&&(o=t.indexOf(e+"=")),t=-1==o?null:(o=t.indexOf("=",o)+1,-1==(n=t.indexOf(";",o))&&(n=t.length),unescape(t.substring(o,n)))}function setCookie(e,n,t){var o=new Date;o.setDate(o.getDate()+t);var i=escape(n)+(null==t?"":"; expires="+o.toUTCString());document.cookie=e+"="+i}function PonerCookie(){setCookie("tiendaaviso","1",365),document.getElementById("barraaceptacion").style.display="none"}"1"!=getCookie("tiendaaviso")&&(document.getElementById("barraaceptacion").style.display="block");
+<script>
+function getCookie(c_name){
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1){
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1){
+        c_value = null;
+    }else{
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1){
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start,c_end));
+    }
+    return c_value;
+}
+ 
+function setCookie(c_name,value,exdays){
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=c_name + "=" + c_value;
+}
+ 
+if(getCookie('tiendaaviso')!="1"){
+    document.getElementById("barraaceptacion").style.display="block";
+}
+function PonerCookie(){
+    setCookie('tiendaaviso','1',365);
+    document.getElementById("barraaceptacion").style.display="none";
+}
+</script>
